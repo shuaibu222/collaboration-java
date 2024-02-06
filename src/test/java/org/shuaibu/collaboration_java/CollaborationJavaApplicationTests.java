e90adb82d4088940dc940ccf1e079607b6b6f910
shuaibu <shuaibuabdulkadir222@gmail.com>
2024-02-06 13:50:16 +0100

    first commit to our collaboration project

---
+++ src/test/java/org/shuaibu/collaboration_java/CollaborationJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shuaibu.collaboration_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborationJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
