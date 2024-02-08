79a126e5086c5194b1b654543f683a719f51beff
shuaibu222 <shuaibuabdulkadir222@gmail.com>
2024-02-08 12:38:36 +0100

    change some conventions

--- src/main/java/org/shuaibu/collaboration_java/products/ProductsController.java
+++ src/main/java/org/shuaibu/collaboration_java/products/ProductsController.java
@@ -10,10 +10,10 @@ import java.util.Optional;
 @RestController
 public class ProductsController {
 
-    public final ProductsRepoInterface productsRepoInterface;
+    public final ProductsRepoInterface PRODUCT_REPO_INTERFACE;
 
     public ProductsController(ProductsRepoInterface productsRepoInterface) {
-        this.productsRepoInterface = productsRepoInterface;
+        this.PRODUCT_REPO_INTERFACE = productsRepoInterface;
     }
 
     // record here for storing the data
@@ -26,36 +26,34 @@ public class ProductsController {
 
     // Get all products
     @GetMapping("/api/v1/products")
-    public List<ProductsModel> getAllProducts() { return productsRepoInterface.findAll(); }
+    public List<ProductsModel> getAllProducts() { return PRODUCT_REPO_INTERFACE.findAll(); }
 
     // Get product by its id
     @GetMapping("/api/v1/products/{id}")
     public Optional<ProductsModel> getById(@PathVariable Integer id){
-        return productsRepoInterface.findById(id);
+        return PRODUCT_REPO_INTERFACE.findById(id);
     }
 
     //  Create a new products
     @PostMapping("/api/v1/products")
-    public ProductsModel createProduct (@RequestBody @Valid Request request) {
-        ProductsModel product = new ProductsModel();
-        product.setName(request.name());
-        return productsRepoInterface.save(product);
+    public ProductsModel createProduct (@RequestBody @Valid ProductsModel product) {
+        return PRODUCT_REPO_INTERFACE.save(product);
     }
 
     // Update an existing product
     @PutMapping("/api/v1/products/{id}")
     public ProductsModel updateProduct(@PathVariable Integer id, @RequestBody Request request){
-        return productsRepoInterface.findById(id)
+        return PRODUCT_REPO_INTERFACE.findById(id)
                 .map(targetProduct -> {
                     targetProduct.setName(request.name());
-                    return productsRepoInterface.save(targetProduct);
+                    return PRODUCT_REPO_INTERFACE.save(targetProduct);
                 }).orElseThrow(() -> new EntityNotFoundException());
     }
 
     // Delete a product by its id
     @DeleteMapping("/api/v1/products/{id}")
     public String deleteProduct(@PathVariable Integer id) {
-        productsRepoInterface.deleteById(id);
+        PRODUCT_REPO_INTERFACE.deleteById(id);
         return "Deleted Successfully";
     }
 
